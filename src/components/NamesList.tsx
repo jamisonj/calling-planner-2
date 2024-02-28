9a6e4913a3fba3ddfd75203ece3b3a2fffef698d
Justin Jamison <justin.jamison@outlook.com>
2024-02-27 18:11:06 -0700

    Set up sample UI component for grouping names by letter.

--- src/components/NamesList.tsx
+++ src/components/NamesList.tsx
@@ -5,17 +5,11 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 import Typography from '@mui/material/Typography';
 
-function generate(element: React.ReactElement) {
-    return [0, 1, 2].map((value) =>
-      cloneElement(element, {
-        key: value,
-      }),
-    );
-  }
-
 function NamesList() {
+    // TODO: Set up importing name list from LDS site.
     const [namesList, setNamesList] = useState([]);
 
     return (
@@ -27,18 +21,35 @@ function NamesList() {
             <Typography sx={{ mt: 2, mb: 2, fontSize: 18 }} variant="h2" component="div">
                 Fort Herriman SA Ward
             </Typography>
-            <List dense={false}>
-            {generate(
-                <ListItem>
-                <ListItemAvatar>
-                    <Avatar></Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Single-line item"
-                    secondary="Secondary text"
-                />
-                </ListItem>,
-            )}
+            <List 
+                dense={false}
+                sx={{
+                    width: '100%',
+                    maxWidth: 360,
+                    bgcolor: 'background.paper',
+                    position: 'relative',
+                    overflow: 'auto',
+                    maxHeight: 300,
+                    '& ul': { padding: 0 },
+                }} >
+                {[0, 1, 2, 3, 4].map((sectionId) => (
+                        <li key={`section-${sectionId}`}>
+                        <ul>
+                            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+                            {[0, 1, 2].map((item) => (
+                            <ListItem key={`item-${sectionId}-${item}`}>
+                                <ListItemAvatar>
+                                    <Avatar></Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={`Item ${item}`}
+                                    secondary="Secondary text"
+                                />
+                            </ListItem>
+                            ))}
+                        </ul>
+                        </li>
+                    ))}
             </List>
         </section>
     );
