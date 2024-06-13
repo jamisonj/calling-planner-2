715dafc67a0bf5285697dd2d44a07f575897d74a
jamisonj <justin.jamison@outlook.com>
2024-06-13 16:06:46 -0600

    Fixed syntax and change to module.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-    res.json({"users": ["userOne", "userTwo"]});
+    res.json({"users": ["userOne", "userTwo"]}); // Dummy code to fill in later.
 })
 
 app.listen(8081, () => console.log('server started on port 8081'));
